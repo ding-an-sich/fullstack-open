6bff2927865a8555c422ae7ee913c93eeced3bfa
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-22 16:42:51 -0300

    Increase counters and display

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <h3>Statistics</h3>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+  
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const inscreaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h3>Give feedback</h3>
+      <Button handleClick={increaseGood} text={'good'}/>
+      <Button handleClick={increaseNeutral} text={'neutral'}/>
+      <Button handleClick={inscreaseBad} text={'bad'}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
