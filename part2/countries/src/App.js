c20b0b162083b95819a82ad08171b282968e4a33
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-25 18:37:23 -0300

    Country list and details

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,10 +9,55 @@ const Filter = (props) => {
   )
 }
 
-const List = ({ countries }) => {
-  console.log(countries)
+const Details = ({ country }) => {
+
   return (
-    null
+    <div>
+      <h2>{country.name}</h2>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <div>
+        <h3>languages</h3>
+        <ul>
+          {country.languages.map(lang =>
+            <li key={lang.iso639_1}>{lang.name}</li>
+            )}
+        </ul>
+      </div>
+      <div>
+        <img src={country.flag} alt={`Flag of ${country.name}`}></img>
+      </div>
+    </div>
+  )
+}
+
+const List = ({ countries, handleSearchFilter }) => {
+  if (countries.length > 1 && countries.length <= 10) {
+    return (
+      <div>
+        {countries.map(country =>
+          <div key={country.name}>
+          <p> {country.name} 
+            <button value={country.name} onClick={handleSearchFilter}>show</button>
+          </p> 
+          </div>
+          )}
+      </div>
+    )
+  } else if (countries.length === 1) {
+    return (
+      <div>
+        <Details country={countries[0]}/>
+      </div>
+    )
+  } else if (countries.length === 0) {
+    return (
+      <p>No matches</p>
+    )
+  }
+
+  return (
+    <p>Too many matches. Specify the search.</p>
   )
 }
 
@@ -30,16 +75,17 @@ const App = () => {
   const handleSearchFilter = (event) => {
     setSearchFilter(event.target.value)
   }
-
+  
+ 
   const countriesList = searchFilter === ''
     ? countries
-    : countries.filter((country) => country.name.includes(searchFilter))
+    : countries.filter((country) => country.name.toUpperCase().includes(searchFilter.toUpperCase()))
 
   return (
     <div>
       <h2>Country info</h2>
       <Filter searchFilter={searchFilter} handleSearchFilter={handleSearchFilter} />
-      <List countries={countriesList} />
+      <List countries={countriesList} handleSearchFilter={handleSearchFilter}/>
     </div>
   )
 }
